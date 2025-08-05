1ab3b1ada4934498d8651c2db7ebf4b374609eef
Кирилл Кушов <kushovk2003@mail.ru>
2025-08-05 16:21:53 +0500

    fix

--- pages/registration/Description.vue
+++ pages/registration/Description.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-  import { Icon } from '@iconify/vue'
+import { Icon } from "@iconify/vue";
 
-  const activeTab = ref<'description' | 'media'>('description')
-  const isInfoOpen = ref(false)
-  const isDescOpen = ref(false)
+const activeTab = ref<"description" | "media">("description");
+const isInfoOpen = ref(false);
+const isDescOpen = ref(false);
 </script>
 <template>
   <section class="container mx-auto px-[16px] py-[50px]">
@@ -13,14 +13,18 @@
         <div class="flex gap-[12px]">
           <h1
             class="text-[22px] font-semibold cursor-pointer"
-            :class="activeTab === 'description' ? 'text-orange01' : 'text-gray01/60'"
-            @click="activeTab = 'description'">
+            :class="
+              activeTab === 'description' ? 'text-orange01' : 'text-gray01/60'
+            "
+            @click="activeTab = 'description'"
+          >
             Описание модели
           </h1>
           <h1
             class="text-[22px] font-semibold cursor-pointer"
             :class="activeTab === 'media' ? 'text-orange01' : 'text-gray01/60'"
-            @click="activeTab = 'media'">
+            @click="activeTab = 'media'"
+          >
             Фото/видео
           </h1>
         </div>
@@ -29,99 +33,76 @@
         <!-- text -->
         <div
           v-if="activeTab === 'description'"
-          class="flex flex-col gap-[14px] lg:text-[20px] md:text-[18px] text-gray01 font-medium">
+          class="flex flex-col gap-[14px] lg:text-[20px] md:text-[18px] text-gray01 font-medium"
+        >
           <!-- mobile -->
           <div class="flex md:hidden">
-            <div v-if="!isDescOpen" @click="isDescOpen = true" class="cursor-pointer">
-              Памятник М12 из карельского гранита габбро-диабаз. Монумент ассоциируется с землёй и стабильностью за счёт
-              своей прямоугольной ...
+            <div
+              v-if="!isDescOpen"
+              @click="isDescOpen = true"
+              class="cursor-pointer"
+            >
+              Памятник М12 из карельского гранита габбро-диабаз. Монумент
+              ассоциируется с землёй и стабильностью за счёт своей прямоугольной
+              ...
               <span class="text-gray01/80">ещё</span>
             </div>
 
-            <div v-else @click="isDescOpen = false" class="flex flex-col gap-[12px] cursor-pointer">
+            <div
+              v-else
+              @click="isDescOpen = false"
+              class="flex flex-col gap-[12px] cursor-pointer"
+            >
               <p>
-                Памятник М12 из карельского гранита габбро-диабаз. Монумент ассоциируется с землёй и стабильностью за
-                счёт своей прямоугольной формы. На фоне строгих линий гармонично смотрится оформление верхней части
-                стелы. Контуры волны переходят в арочный изгиб — словно размеренное течение жизни нашло свое дальнейшее
-                продолжение на небесном своде. Подобное оформление придаёт памятнику лёгкость и мягкость
+                Памятник М12 из карельского гранита габбро-диабаз. Монумент
+                ассоциируется с землёй и стабильностью за счёт своей
+                прямоугольной формы. На фоне строгих линий гармонично смотрится
+                оформление верхней части стелы. Контуры волны переходят в
+                арочный изгиб — словно размеренное течение жизни нашло свое
+                дальнейшее продолжение на небесном своде. Подобное оформление
+                придаёт памятнику лёгкость и мягкость
               </p>
               <p>
-                Памятник из габбро-диабаза обладает красивой текстурой и глубоким тёмным цветом. Полировка поверхности
-                придаёт ему зеркальный блеск и элегантный вид. На данном памятнике эффектно будет смотреться портрет и
-                любая гравировка
+                Памятник из габбро-диабаза обладает красивой текстурой и
+                глубоким тёмным цветом. Полировка поверхности придаёт ему
+                зеркальный блеск и элегантный вид. На данном памятнике эффектно
+                будет смотреться портрет и любая гравировка
               </p>
-              <p class="text-gray01/60">Все параметры можно корректировать и выбрать оптимальную цену</p>
+              <p class="text-gray01/60">
+                Все параметры можно корректировать и выбрать оптимальную цену
+              </p>
             </div>
           </div>
 
           <!-- desktop -->
           <div class="hidden md:flex flex-col gap-[12px]">
             <p>
-              Памятник М12 из карельского гранита габбро-диабаз. Монумент ассоциируется с землёй и стабильностью за счёт
-              своей прямоугольной формы. На фоне строгих линий гармонично смотрится оформление верхней части стелы.
-              Контуры волны переходят в арочный изгиб — словно размеренное течение жизни нашло свое дальнейшее
-              продолжение на небесном своде. Подобное оформление придаёт памятнику лёгкость и мягкость
+              Памятник М12 из карельского гранита габбро-диабаз. Монумент
+              ассоциируется с землёй и стабильностью за счёт своей прямоугольной
+              формы. На фоне строгих линий гармонично смотрится оформление
+              верхней части стелы. Контуры волны переходят в арочный изгиб —
+              словно размеренное течение жизни нашло свое дальнейшее продолжение
+              на небесном своде. Подобное оформление придаёт памятнику лёгкость
+              и мягкость
             </p>
             <p>
-              Памятник из габбро-диабаза обладает красивой текстурой и глубоким тёмным цветом. Полировка поверхности
-              придаёт ему зеркальный блеск и элегантный вид. На данном памятнике эффектно будет смотреться портрет и
-              любая гравировка
+              Памятник из габбро-диабаза обладает красивой текстурой и глубоким
+              тёмным цветом. Полировка поверхности придаёт ему зеркальный блеск
+              и элегантный вид. На данном памятнике эффектно будет смотреться
+              портрет и любая гравировка
             </p>
-            <p class="text-gray01/60">Все параметры можно корректировать и выбрать оптимальную цену</p>
+            <p class="text-gray01/60">
+              Все параметры можно корректировать и выбрать оптимальную цену
+            </p>
           </div>
         </div>
 
         <!-- media -->
-        <div v-else class="flex items-center flex-col gap-[14px] lg:text-[20px] md:text-[18px] text-gray01 font-medium">
+        <div
+          v-else
+          class="flex items-center flex-col gap-[14px] lg:text-[20px] md:text-[18px] text-gray01 font-medium"
+        >
           <p>Тут будут фото и видео памятника</p>
-        </div>
-      </div>
-      <!-- right -->
-      <div class="flex items-center flex-col gap-[16px]">
-        <div class="flex items-center cursor-pointer select-none" @click="isInfoOpen = !isInfoOpen">
-          <h1 class="text-[20px] text-orange01 font-semibold">Доп. информация</h1>
-          <Icon
-            icon="meteor-icons:chevron-down"
-            width="24"
-            height="24"
-            :style="{ color: '#EC631B' }"
-            :class="[
-              'transition-transform duration-300 ml-2 block md:hidden',
-              { 'rotate-0': isInfoOpen, 'rotate-[-90deg]': !isInfoOpen }
-            ]" />
-        </div>
-        <!-- mobile -->
-        <div v-show="isInfoOpen" class="flex flex-col gap-[12px] transition-all duration-300 md:hidden">
-          <div>
-            <h1 class="text-[20px] text-black01 font-semibold">Размеры:</h1>
-          </div>
-          <div class="flex flex-col gap-[4px] text-[18px] text-gray01 font-medium">
-            <p>80*40*8 см</p>
-            <p>90*45*8 см</p>
-            <p>100*45*8 см</p>
-            <p>100*50*8 см</p>
-            <p>110*55*8 см</p>
-            <p>120*60*8 см</p>
-            <p>120*70*8 см</p>
-            <p>140*70*8 см</p>
-          </div>
-        </div>
-
-        <!-- desktop -->
-        <div class="flex-col gap-[12px] transition-all duration-300 hidden md:flex">
-          <div>
-            <h1 class="text-[20px] text-black01 font-semibold">Размеры:</h1>
-          </div>
-          <div class="flex flex-col gap-[4px] text-[18px] text-gray01 font-medium">
-            <p>80*40*8 см</p>
-            <p>90*45*8 см</p>
-            <p>100*45*8 см</p>
-            <p>100*50*8 см</p>
-            <p>110*55*8 см</p>
-            <p>120*60*8 см</p>
-            <p>120*70*8 см</p>
-            <p>140*70*8 см</p>
-          </div>
         </div>
       </div>
     </div>
--- pages/registration/Item.vue
+++ pages/registration/Item.vue
@@ -3,7 +3,6 @@ import { ref } from "vue";
 import { useRoute } from "vue-router";
 import { Icon } from "@iconify/vue";
 import catalogItems from "~/composables/home/catalogItems/catalogItems";
-import registrationItems from "~/composables/registration/registrationItem";
 import registrationGroups from "~/composables/registration/registrationItem";
 
 const route = useRoute();
@@ -18,23 +17,22 @@ const selectedImage = ref(product?.images?.[0] ?? product?.imgPath ?? "");
 const hoveredIcon1 = ref(false);
 const hoveredIcon3 = ref(false);
 
-const isOpenStela = ref(false);
-
-function toggleStela() {
-  isOpenStela.value = !isOpenStela.value;
+const isOpenDecoration = ref(false);
+function toggleDecoration() {
+  isOpenDecoration.value = !isOpenDecoration.value;
 }
-const isOpenPost = ref(false);
 
-function togglePost() {
-  isOpenPost.value = !isOpenPost.value;
+const isOpenPortret = ref(false);
+function togglePortret() {
+  isOpenPortret.value = !isOpenPortret.value;
 }
+
 const isOpenPlit = ref(false);
-
 function togglePlit() {
   isOpenPlit.value = !isOpenPlit.value;
 }
-const isOpenMaterial = ref(false);
 
+const isOpenMaterial = ref(false);
 function toggleMaterial() {
   isOpenMaterial.value = !isOpenMaterial.value;
 }
@@ -92,7 +90,7 @@ function toggleMaterial() {
         <p class="text-[16px] text-black01 font-normal font-sans">Оформление</p>
       </div>
 
-      <div class="flex flex-col lg:flex-row w-full gap-[32px] items-center">
+      <div class="flex flex-col lg:flex-row w-full gap-[32px]">
         <!-- left -->
         <div class="flex gap-[24px]">
           <!-- images -->
@@ -120,83 +118,192 @@ function toggleMaterial() {
         </div>
 
         <!-- right -->
-        <div>
-          <!-- 1 -->
-          <div
-            class="flex items-center justify-between w-[880px] border py-[16px] px-[24px] rounded-[8px] cursor-pointer"
-          >
-            <div class="flex flex-col gap-[6px]">
-              <p class="text-[18px] text-gray01 font-normal">
-                Сторона оформления
-              </p>
+        <div class="flex flex-col gap-[24px]">
+          <div class="flex flex-col gap-[16px]">
+            <!-- 1 -->
+            <div
+              @click="toggleDecoration"
+              class="flex items-center justify-between w-[880px] border hover:border-orange01/50 transition-all duration-300 py-[16px] px-[24px] rounded-[8px] cursor-pointer"
+            >
+              <div class="flex flex-col gap-[6px]">
+                <p class="text-[18px] text-gray01 font-normal">
+                  Сторона оформления
+                </p>
+              </div>
+              <div>
+                <Icon
+                  icon="meteor-icons:chevron-down"
+                  width="24"
+                  height="24"
+                  :style="{ color: '#9DA1AA' }"
+                />
+              </div>
             </div>
-            <div>
-              <Icon
-                icon="meteor-icons:chevron-down"
-                width="24"
-                height="24"
-                :style="{ color: '#9DA1AA' }"
-              />
+            <!-- 1 - 1 -->
+            <div
+              v-if="isOpenDecoration"
+              class="border border-t-0 rounded-b-[8px] py-[16px] px-[24px] flex flex-col gap-[16px]"
+            >
+              <div>
+                <p class="text-[18px] text-gray01 font-medium">
+                  Выберите сторону памятника, которую хотите оформить
+                </p>
+              </div>
+              <div>
+                <div>
+                  <button
+                    class="px-[16px] py-[12px] rounded-[8px] bg-orange01/[6%] text-[18px] text-black01 font-semibold"
+                  >
+                    Лицевая сторона стелы
+                  </button>
+                </div>
+                <div class="flex items-end justify-end">
+                  <button
+                    class="px-[12px] py-[6px] rounded-[6px] bg-orange01/10 text-[16px] text-orange01 font-medium"
+                  >
+                    Далее
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <!-- 2 -->
+            <div
+              @click="togglePortret"
+              class="flex items-center justify-between w-[880px] border py-[16px] px-[24px] rounded-[8px] cursor-pointer"
+            >
+              <div class="flex flex-col gap-[6px]">
+                <p class="text-[18px] text-gray01 font-normal">Портрет</p>
+              </div>
+              <div>
+                <Icon
+                  icon="meteor-icons:chevron-down"
+                  width="24"
+                  height="24"
+                  :style="{ color: '#9DA1AA' }"
+                />
+              </div>
+            </div>
+            <!-- 2 - 1 -->
+            <div
+              v-if="isOpenPortret"
+              class="border border-t-0 rounded-b-[8px] py-[16px] px-[24px] flex flex-col gap-[16px]"
+            >
+              <div>
+                <p class="text-[18px] text-gray01 font-medium">
+                  Загрузите необходимую фотографию
+                </p>
+              </div>
+              <div class="flex flex-col gap-[16px]">
+                <div>
+                  <button
+                    class="flex items-center justify-center gap-[8px] px-[16px] py-[12px] rounded-[8px] bg-orange01 text-[16px] text-white font-medium"
+                  >
+                    <img
+                      src="/public/images/registration/item/icon1.svg"
+                      alt=""
+                    />
+                    Загрузить фото
+                  </button>
+                </div>
+                <div class="flex items-end justify-between">
+                  <div class="w-1/2">
+                    <p class="text-[16px] text-gray01/60 font-medium">
+                      *Обратите внимание, не все изображения могут подойти для
+                      нанесения. <br />При оформлении заказа менеджер подробно
+                      Вас проконсультирует
+                    </p>
+                  </div>
+                  <div class="flex items-center justify-center gap-[8px]">
+                    <button class="p-[10px] rounded-[6px] bg-orange01/10">
+                      <Icon
+                        icon="material-symbols:arrow-left-alt-rounded"
+                        width="18"
+                        height="18"
+                        class="text-orange01"
+                      />
+                    </button>
+                    <button
+                      class="px-[12px] py-[8px] rounded-[6px] bg-orange01/10 text-[16px] text-orange01 font-medium"
+                    >
+                      Далее
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- 3 -->
+            <div
+              class="flex items-center justify-between w-[880px] border py-[16px] px-[24px] rounded-[8px] cursor-pointer"
+            >
+              <div class="flex flex-col gap-[6px]">
+                <p class="text-[18px] text-gray01 font-normal">
+                  Фамилия Имя Отчество
+                </p>
+              </div>
+              <div>
+                <Icon
+                  icon="meteor-icons:chevron-down"
+                  width="24"
+                  height="24"
+                  :style="{ color: '#9DA1AA' }"
+                />
+              </div>
+            </div>
+
+            <!-- 4 -->
+            <div
+              class="flex items-center justify-between w-[880px] border py-[16px] px-[24px] rounded-[8px] cursor-pointer"
+            >
+              <div class="flex flex-col gap-[6px]">
+                <p class="text-[18px] text-gray01 font-normal">Памятные даты</p>
+              </div>
+              <div>
+                <Icon
+                  icon="meteor-icons:chevron-down"
+                  width="24"
+                  height="24"
+                  :style="{ color: '#9DA1AA' }"
+                />
+              </div>
+            </div>
+
+            <!-- 5 -->
+            <div
+              class="flex items-center justify-between w-[880px] border py-[16px] px-[24px] rounded-[8px] cursor-pointer"
+            >
+              <div class="flex flex-col gap-[6px]">
+                <p class="text-[18px] text-gray01 font-normal">
+                  Текст эпитафии
+                </p>
+              </div>
+              <div>
+                <Icon
+                  icon="meteor-icons:chevron-down"
+                  width="24"
+                  height="24"
+                  :style="{ color: '#9DA1AA' }"
+                />
+              </div>
             </div>
           </div>
-
-          <!-- 2 -->
-          <div
-            class="flex items-center justify-between w-[880px] border py-[16px] px-[24px] rounded-[8px] cursor-pointer"
-          >
-            <div class="flex flex-col gap-[6px]">
-              <p class="text-[18px] text-gray01 font-normal">Стела</p>
-              <h2 class="text-[20px] text-black01 font-semibold">
-                Размер 80*40*8 см
-              </h2>
-            </div>
-            <div>
+          <!-- btn -->
+          <div class="flex items-center gap-[12px]">
+            <div class="py-[12px] px-[16px] rounded-[6px] bg-orange01/10">
               <Icon
-                icon="meteor-icons:chevron-down"
+                icon="basil:arrow-left-outline"
                 width="24"
                 height="24"
-                :style="{ color: '#9DA1AA' }"
+                class="text-orange01"
               />
-            </div>
-          </div>
-
-          <!-- 3 -->
-          <div
-            class="flex items-center justify-between w-[880px] border py-[16px] px-[24px] rounded-[8px] cursor-pointer"
-          >
-            <div class="flex flex-col gap-[6px]">
-              <p class="text-[18px] text-gray01 font-normal">Стела</p>
-              <h2 class="text-[20px] text-black01 font-semibold">
-                Размер 80*40*8 см
-              </h2>
             </div>
             <div>
-              <Icon
-                icon="meteor-icons:chevron-down"
-                width="24"
-                height="24"
-                :style="{ color: '#9DA1AA' }"
-              />
-            </div>
-          </div>
-
-          <!-- 4 -->
-          <div
-            class="flex items-center justify-between w-[880px] border py-[16px] px-[24px] rounded-[8px] cursor-pointer"
-          >
-            <div class="flex flex-col gap-[6px]">
-              <p class="text-[18px] text-gray01 font-normal">Стела</p>
-              <h2 class="text-[20px] text-black01 font-semibold">
-                Размер 80*40*8 см
-              </h2>
-            </div>
-            <div>
-              <Icon
-                icon="meteor-icons:chevron-down"
-                width="24"
-                height="24"
-                :style="{ color: '#9DA1AA' }"
-              />
+              <button
+                class="py-[12px] px-[16px] rounded-[6px] bg-orange01 text-white text-[18px] font-medium font-sans"
+              >
+                Сохранить и перейти к результату
+              </button>
             </div>
           </div>
         </div>
